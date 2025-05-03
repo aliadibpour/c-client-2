263772fb9bbc40a9e3f6eca62673995bc3b6b077
aliadibpour <ali.adp0101@gmail.com>
2025-05-03 10:56:02 +0330

    improve structure and add navigation

--- App.tsx
+++ App.tsx
@@ -1,162 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, { useEffect } from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  Button,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  TouchableOpacity,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-import { NativeModules } from 'react-native';
-import TdLib from 'react-native-tdlib';
 function App(): React.JSX.Element {
 
-const { MyMathModule } = NativeModules;
-
-async function initializeTdLib() {
-  console.log("hello man this is tdlib")
-  // Start TDLib
-await TdLib.startTdLib({
-    api_id: 19661737,
-    api_hash: '28b0dd4e86b027fd9a2905d6c343c6bb'
-  });
-
-  // Login with phone number
-  await TdLib.login({
-    countrycode: '+98',
-    phoneNumber: '9924508531'
-  });
-
-  // Verify phone number
-  await TdLib.verifyPhoneNumber('12345'); // Replace with the OTP you received
-
-  // Verify password (Optional)
-  await TdLib.verifyPassword('password');
-
-  // Get current profile
-  const profile = await TdLib.getProfile();
-  console.log(profile);
-}
-
-
-useEffect(() => {
-  console.log("alli")
-  MyMathModule.addNumbers(3, 5).then((result: any) => console.log('Result:', result));
-})
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const safePadding = '5%';
-
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-          <TouchableOpacity onPress={() => initializeTdLib()}>
-            <Text>the test</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+    <View>
+       <Text>The Corner</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
 });
 
 export default App;
---
+++ src/navigation/RootNavigatore.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AuthNavigator from './AuthNavigator';
+import TabNavigator from './TabNavigator';
+
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  const isLoggedIn = false;
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {isLoggedIn ? (
+        <Stack.Screen name="Main" component={TabNavigator} />
+      ) : (
+        <Stack.Screen name="Auth" component={AuthNavigator} />
+      )}
+    </Stack.Navigator>
+  );
+}
