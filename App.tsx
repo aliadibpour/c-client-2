8a0df048633a03312d093e4d6078b563e1b82833
aliadibpour <ali.adp0101@gmail.com>
2025-05-03 10:10:06 +0330

    create src for imporve structure folders

--- App.tsx
+++ App.tsx
@@ -98,15 +98,6 @@ useEffect(() => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
   const safePadding = '5%';
 
   return (
